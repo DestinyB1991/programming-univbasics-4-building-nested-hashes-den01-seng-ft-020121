ea9c47414b463cb83ee65c538f9a0f22f03411ac
Destiny Boling <bolingdestinyr@gmail.com>
2021-01-25 18:10:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,12 +1,25 @@
 def base_hash
-  monopoly = { :railraods => {}
-  }
+  monopoly =  {
+    :railroads=>
+    {
+      {
+  
+      }
 end
 
 
 def monopoly_with_second_tier
-  monopoly = {:railraods => {}
-  }
+  monopoly =  {
+    :railroads=>
+    {
+      :pieces=>4,
+      :rent_in_dollars=>
+      {
+        :one_piece_owned=>25,
+        :two_pieces_owned=>50,
+        :three_pieces_owned=>100,
+        :four_pieces_owned=>200
+      }
 end
 
 def monopoly_with_third_tier
